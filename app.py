bab66fc3f7e8f85a5fca55b396134f614410eb3b
panwire <74929550+panwire@users.noreply.github.com>
2023-03-04 10:57:48 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route("/")
-def hello_world():
-  return "Hello, Tamichh"
+def hello_home():
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
